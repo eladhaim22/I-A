697dc0026ec00711d050c2c17dccc0b6c2c14149
Elad Haim <eladhaim22@gmail.com>
2018-08-13 19:12:33 -0300

    Initial commit

---
+++ src/main/java/com/ia/ia/IaApplication.java
@@ -0,0 +1,12 @@
+package com.ia.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaApplication.class, args);
+	}
+}
